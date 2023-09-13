f57f3c6464c228d4eb4ad6d7c70ac9c83a9bfec6
LEEJAEBOK-KR <gets@naver.com>
2023-09-13 17:49:26 +0900

    1st

--- Assets/Scenes/test1.cs
+++ Assets/Scenes/test1.cs
@@ -7,7 +7,7 @@ public class test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Logging")
     }
 
     // Update is called once per frame
